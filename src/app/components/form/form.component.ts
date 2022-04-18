87bffaee9ba07ddf121e5a0857f02f9551181407
MutheuLinet <muneenilinet@gmail.com>
2022-04-18 15:59:51 +0300

    Add search function

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, Output,   EventEmitter } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
+import { User } from 'src/app/classes/user';
+import { Repository } from 'src/app/classes/repository';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  providers: [ GithubService ]
 })
-export class FormComponent implements OnInit {
 
-  constructor() { }
+export class FormComponent implements OnInit {
+  _user!: User;
+  _repo: Repository[]=[];
+
+
+  constructor( public searchService: GithubService) { }
+
+  searchUser(searchName:string){
+    if (searchName !=='') {
+      this.searchService.getUser(searchName)
+      console.log(searchName)
+    };
+  };
+
+  
+  searchRepos(searchName:string){
+    if (searchName !=='') {
+      this.searchService.getRepositories(searchName)
+      console.log(searchName)
+    };
+  };
 
   ngOnInit(): void {
+    this._user = this.searchService._user;
+    this._repo = this.searchService._repositoryItems;
+    this.searchUser("MutheuLinet");
+    this.searchRepos("MutheuLinet");
+
   }
+
+
+
 
 }
