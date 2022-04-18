26ea8ca79299679853738d460184a58274732c1c
MutheuLinet <muneenilinet@gmail.com>
2022-04-18 15:55:27 +0300

    Add repository constructor

--- src/app/classes/repository.ts
+++ src/app/classes/repository.ts
@@ -1,2 +1,12 @@
 export class Repository {
+    constructor(public repoName: string, public description: string, public language:string, public html_url: string){
+
+    }
 }
+// export class Repos {
+//     constructor(public name: string, public description: string, public created_at: Date, 
+
+// public html_url: string, public forks: number, public watchers_count:number, public language:string) {
+
+//     }
+// }
