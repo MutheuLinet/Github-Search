9b2107cf57d962a31876de76d5229ecd3ff6e615
MutheuLinet <muneenilinet@gmail.com>
2022-04-18 16:24:39 +0300

    Edit not-found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { RepoFormComponent } from './components/repo-form/repo-form.component';
 import { FormComponent } from './components/form/form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: 'user', component: FormComponent},
   { path: 'repo', component: RepoFormComponent},
   { path: '', redirectTo:"/user", pathMatch:"full"},
+  { path:'**', component: NotFoundComponent},
+
 
 
 ];
