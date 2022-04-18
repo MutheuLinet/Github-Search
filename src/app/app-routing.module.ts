795cdabf2f15fdf9c3ba9605f17458ef134418da
MutheuLinet <muneenilinet@gmail.com>
2022-04-18 15:48:16 +0300

    Add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { RepoFormComponent } from './components/repo-form/repo-form.component';
+import { FormComponent } from './components/form/form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'user', component: FormComponent},
+  { path: 'repo', component: RepoFormComponent},
+  { path: '', redirectTo:"/user", pathMatch:"full"},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
