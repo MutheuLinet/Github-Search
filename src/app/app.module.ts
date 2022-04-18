8412a3ed9e31cf7d2f79f0eb5ab4053068189888
MutheuLinet <muneenilinet@gmail.com>
2022-04-18 15:51:31 +0300

    Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RepoFormComponent } from './components/repo-form/repo-form.component';
+import { RepoSearchService } from './services/repo-search.service';
+import { GithubService } from './services/github.service';
+import { HighlightDirective } from './directives/highlight.directive';
+import { DateChangerPipe } from './pipes/date-changer.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    NavbarComponent
+    NavbarComponent,
+    RepoFormComponent,
+    HighlightDirective,
+    DateChangerPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    RepoSearchService,
+    GithubService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
